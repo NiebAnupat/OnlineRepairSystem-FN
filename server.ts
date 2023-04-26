88d34cf767abdd4761851f72ccd368547d8b26ad
NiebAnupat <anupat.k64@rsu.ac.th>
2023-04-26 07:20:25 +0700

    fear(server) : update for deployment

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import { parse } from 'url'
 import next from 'next'
-const hostname = 'http://anupat-dav.com'
+const hostname = 'localhost'
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev,hostname,port })
